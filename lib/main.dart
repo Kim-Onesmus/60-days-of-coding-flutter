e933d8521047f76b8c42771e117f4a7b23367f47
Muimi <kimones1122@gmail.com>
2023-06-12 19:49:55 +0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MaterialApp(
+void main() => runApp(MaterialApp(
     home: Scaffold(
       appBar: AppBar(
-        
+        title: Text('My first App'),
       ),
     ),
   ));
-}
